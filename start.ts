2a080fa28dffdb353e071b0f2727d6b31ff299b7
derek williams <derek61@gmail.com>
2023-03-25 15:39:20 -0600

    copy starting code from main to start

--- start.ts
+++ start.ts
@@ -1,3 +1,13 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
 
-serve((req: Request) => new Response("Hello World"));
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
